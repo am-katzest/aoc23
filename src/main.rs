7d239e9bff4690e3ccfac72d94d2be044ab89e68
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-02 03:36:36 +0100

    works!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::{fs::read_to_string };
+use std::fs::read_to_string;
 
+fn parta(f: &str) -> i32 {
+    read_to_string(f)
+    .unwrap()
+    .lines()
+    .map(edge_digits)
+    .map(dgts_to_int)
+    .sum()
+}
 fn main() {
-    read_to_string("1a.input")
-        .unwrap()
-        .lines()
-        .for_each(|x| println!("{}\n", x));
+    println!("part a: {}", parta("1a.input"));
 }
 
 fn is_digit(x: &char) -> bool {
@@ -16,10 +21,19 @@ fn edge_digits(x: &str) -> (char,char) {
      x.chars().rev().find(is_digit).unwrap())
 }
 
+fn char_to_int(a:char) -> i32 {
+    (a.to_digit(10).unwrap() - '0'.to_digit(10).unwrap()) as i32
+}
+
+fn dgts_to_int(a: (char, char)) -> i32 {
+    match a{
+        (tens, digit) => char_to_int(tens) * 10 + char_to_int(digit)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::{is_digit, edge_digits};
+    use crate::{is_digit, edge_digits, char_to_int, dgts_to_int, parta};
 
     #[test]
     fn is_digit_test() {
@@ -40,5 +54,15 @@ mod tests {
         assert_eq!(('2','3'), edge_digits("23"));
         assert_eq!(('2','3'), edge_digits("meow2meow3meow"));
 
+    }
+    #[test]
+    fn to_int_test() {
+        assert_eq!(3, char_to_int('3'));
+        assert_eq!(33, dgts_to_int(('3','3')));
+        assert_eq!(30, dgts_to_int(('3','0')));
+    }
+    #[test]
+    fn parta_test() {
+        assert_eq!(142, parta("1a.input"))
     }
 }
