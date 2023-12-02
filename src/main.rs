cb429a088ad59836b8fe7fe43237b1d2c38160e2
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-02 03:12:54 +0100

    me likey this language

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,38 @@ fn main() {
         .for_each(|x| println!("{}\n", x));
 }
 
-fn is_digit(x: char) -> bool {
-    x >= '0' && x <= '9'
+fn is_digit(x: &char) -> bool {
+    x >= &'0' && x <= &'9'
 }
 
-//fn firstdgt(x: &str) -> Some(char) {
-//    x.chars().into_iter().find(is_digit).map(|x| x.clone())
-//}
+fn edge_digits(x: &str) -> (char,char) {
+    (x.chars().find(is_digit).unwrap(),
+     x.chars().rev().find(is_digit).unwrap())
+}
 
 
 #[cfg(test)]
 mod tests {
-    use crate::is_digit;
+    use crate::{is_digit, edge_digits};
 
     #[test]
     fn is_digit_test() {
-        assert!(is_digit('0'));
-        assert!(is_digit('9'));
-        assert!(is_digit('3'));
-        assert!(is_digit('4'));
-        assert!(!is_digit('a'));
-        assert!(!is_digit(':'));
-        assert!(!is_digit('/'));
+        assert!(is_digit(&'0'));
+        assert!(is_digit(&'9'));
+        assert!(is_digit(&'3'));
+        assert!(is_digit(&'4'));
+        assert!(!is_digit(&'a'));
+        assert!(!is_digit(&':'));
+        assert!(!is_digit(&'/'));
+    }
+    #[test]
+    fn firstdgt_test() {
+        assert_eq!(('1','1'), edge_digits("1"));
+        assert_eq!(('1','1'), edge_digits("meow1"));
+        assert_eq!(('1','1'), edge_digits("1meow"));
+        assert_eq!(('1','1'), edge_digits("1meow1"));
+        assert_eq!(('2','3'), edge_digits("23"));
+        assert_eq!(('2','3'), edge_digits("meow2meow3meow"));
+
     }
 }
