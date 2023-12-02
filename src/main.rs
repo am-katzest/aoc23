09dade232a35f440572f486ffb90868521477f8d
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-02 05:33:38 +0100

    fix types

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
 use std::fs::read_to_string;
 
-static JUST_DIGITS: &[(char, &str)] = &[
-    ('0', "0"),
-    ('1', "1"),
-    ('2', "2"),
-    ('3', "3"),
-    ('4', "4"),
-    ('5', "5"),
-    ('6', "6"),
-    ('7', "7"),
-    ('8', "8"),
-    ('9', "9"),
+static JUST_DIGITS: &[(i32, &str)] = &[
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+    (6, "6"),
+    (7, "7"),
+    (8, "8"),
+    (9, "9"),
 ];
 
-static DIGITS_AND_STRINGS: &[(char, &str)] = &[
-    ('0', "0"), ('0', "zero"),
-    ('1', "1"), ('1', "one"),
-    ('2', "2"), ('2', "two"),
-    ('3', "3"), ('3', "three"),
-    ('4', "4"), ('4', "four"),
-    ('5', "5"), ('5', "five"),
-    ('6', "6"), ('6', "six"),
-    ('7', "7"), ('7', "seven"),
-    ('8', "8"), ('8', "eight"),
-    ('9', "9"), ('9', "nine"),
+static DIGITS_AND_STRINGS: &[(i32, &str)] = &[
+    (0, "0"), (0, "zero"),
+    (1, "1"), (1, "one"),
+    (2, "2"), (2, "two"),
+    (3, "3"), (3, "three"),
+    (4, "4"), (4, "four"),
+    (5, "5"), (5, "five"),
+    (6, "6"), (6, "six"),
+    (7, "7"), (7, "seven"),
+    (8, "8"), (8, "eight"),
+    (9, "9"), (9, "nine"),
 ];
 
 #[derive(Clone, Copy)]
@@ -32,7 +32,7 @@ enum End {
     Right,
 }
 
-fn solve(matchers: &[(char, &str)], f: &str) -> i32 {
+fn solve(matchers: &[(i32, &str)], f: &str) -> i32 {
     read_to_string(f)
         .unwrap()
         .lines()
@@ -46,16 +46,12 @@ fn main() {
     println!("part 2: {}", solve(DIGITS_AND_STRINGS, "1b.input"));
 }
 
-fn char_to_int(a: char) -> i32 {
-    (a.to_digit(10).unwrap() - '0'.to_digit(10).unwrap()) as i32
-}
-
-fn dgts_to_int(pair: (char, char)) -> i32 {
+fn dgts_to_int(pair: (i32, i32)) -> i32 {
     let (tens, ones) = pair;
-    char_to_int(tens) * 10 + char_to_int(ones)
+    tens * 10 + ones
 }
 
-fn index(end: End, string: &str, matcher: (char, &str)) -> Option<(char, usize)> {
+fn index(end: End, string: &str, matcher: (i32, &str)) -> Option<(i32, usize)> {
     let (c, sub) = matcher;
     match end {
         End::Left => string.find(sub).map(|position| (c, position)),
@@ -67,7 +63,7 @@ fn index(end: End, string: &str, matcher: (char, &str)) -> Option<(char, usize)>
     }
 }
 
-fn select(matchers: &[(char, &str)], string: &str, end: End) -> char {
+fn select(matchers: &[(i32, &str)], string: &str, end: End) -> i32 {
     matchers
         .iter()
         .filter_map(|m| index(end, string, *m))
@@ -76,7 +72,7 @@ fn select(matchers: &[(char, &str)], string: &str, end: End) -> char {
         .0
 }
 
-fn edge_digits(matchers: &[(char, &str)], x: &str) -> (char, char) {
+fn edge_digits(matchers: &[(i32, &str)], x: &str) -> (i32, i32) {
     (
         select(matchers, x, End::Left),
         select(matchers, x, End::Right),
@@ -89,9 +85,8 @@ mod tests {
 
     #[test]
     fn to_int_test() {
-        assert_eq!(3, char_to_int('3'));
-        assert_eq!(33, dgts_to_int(('3', '3')));
-        assert_eq!(30, dgts_to_int(('3', '0')));
+        assert_eq!(33, dgts_to_int((3, 3)));
+        assert_eq!(30, dgts_to_int((3, 0)));
     }
     #[test]
     fn parta_test() {
@@ -103,21 +98,21 @@ mod tests {
     }
     #[test]
     fn index_test() {
-        assert_eq!(Some(('3', 0)), index(End::Left, "meow", ('3', "me")));
-        assert_eq!(Some(('3', 2)), index(End::Left, "bemeow", ('3', "me")));
-        assert_eq!(None, index(End::Left, "bemeow", ('3', "niema")));
-        assert_eq!(Some(('3', 0)), index(End::Right, "bemeow", ('3', "ow")));
-        assert_eq!(Some(('3', 1)), index(End::Right, "bemeow", ('3', "eo")));
+        assert_eq!(Some((3, 0)), index(End::Left, "meow", (3, "me")));
+        assert_eq!(Some((3, 2)), index(End::Left, "bemeow", (3, "me")));
+        assert_eq!(None, index(End::Left, "bemeow", (3, "niema")));
+        assert_eq!(Some((3, 0)), index(End::Right, "bemeow", (3, "ow")));
+        assert_eq!(Some((3, 1)), index(End::Right, "bemeow", (3, "eo")));
     }
     #[test]
     fn select_test() {
-        assert_eq!('3', select(DIGITS_AND_STRINGS, "3", End::Right));
-        assert_eq!('1', select(DIGITS_AND_STRINGS, "3meow1", End::Right));
-        assert_eq!('3', select(DIGITS_AND_STRINGS, "3meow1", End::Left));
-        assert_eq!('1', select(DIGITS_AND_STRINGS, "onexxxxxtwo", End::Left));
-        assert_eq!('2', select(DIGITS_AND_STRINGS, "onexxxxxtwo", End::Right));
-        assert_eq!('6', select(JUST_DIGITS, "onex56xxtwo", End::Right));
-        assert_eq!('5', select(JUST_DIGITS, "onex56xxtwo", End::Left));
+        assert_eq!(3, select(DIGITS_AND_STRINGS, "3", End::Right));
+        assert_eq!(1, select(DIGITS_AND_STRINGS, "3meow1", End::Right));
+        assert_eq!(3, select(DIGITS_AND_STRINGS, "3meow1", End::Left));
+        assert_eq!(1, select(DIGITS_AND_STRINGS, "onexxxxxtwo", End::Left));
+        assert_eq!(2, select(DIGITS_AND_STRINGS, "onexxxxxtwo", End::Right));
+        assert_eq!(6, select(JUST_DIGITS, "onex56xxtwo", End::Right));
+        assert_eq!(5, select(JUST_DIGITS, "onex56xxtwo", End::Left));
     }
 
 }
