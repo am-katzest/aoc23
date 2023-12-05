4b79e87459821be824e1b448b02ada743b9f8c6b
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-05 11:43:30 +0100

    parsing done i think

--- src/5.rs
+++ src/5.rs
@@ -1,5 +1,13 @@
 use std::{fs::read_to_string};
 
+use itertools::Itertools;
+
+fn parse_line_of_numbers(l: &str) -> Vec<u32> {
+    l.split_whitespace()
+        .filter_map(|x| x.parse::<u32>().ok())
+        .collect()
+}
+
 #[derive(Clone, Debug, PartialEq)]
 struct MapLine {
     dest: u32,
@@ -15,25 +23,42 @@ fn try_apply_mapping(i: u32, m: MapLine) -> Option<u32> {
     }
 }
 
-fn parse_subline(l: &str) -> Vec<usize> {
-    l.split_whitespace()
-        .filter_map(|x| x.parse::<usize>().ok())
-        .collect()
+fn apply_mappings(i:u32, ms: Vec<MapLine>) -> u32 {
+    for m in ms {
+        match try_apply_mapping(i, m) {
+            Some(o) => return o,
+            _ => (),
+        }
+    }
+    i
 }
 
-fn parse(f: &str) {
-    read_to_string(f)
-        .unwrap()
-        .lines()
-        .for_each(|x| println!("{}", x))
+fn parse_mapping(f: &str) -> MapLine {
+    let (dest, src, len) = parse_line_of_numbers(f).iter().copied().collect_tuple().unwrap();
+    MapLine {dest, src, len}
+}
+
+fn parse_section(f:&str) -> Vec<MapLine> {
+   f.split("\n").skip(1).filter(|x| x.len() > 0).map(parse_mapping).collect()
+}
+
+fn parse(f: &str) -> (Vec<u32>, Vec<Vec<MapLine>>){
+    let s = read_to_string(f).unwrap();
+    let mut i = s.split("\n\n");
+    let seeds = parse_line_of_numbers(i.next().unwrap());
+    let mappings = i.map(parse_section).collect_vec();
+    mappings.iter().for_each(|x| println!("{:?}", x));
+
+    (seeds, mappings)
 }
 
 fn solve(f: &str) -> usize {
+    parse(f);
     3
 }
 
 fn main() {
-    println!("part 1: {}", solve("inputs/4b"));
+    println!("part 1: {}", solve("inputs/5a"));
     //println!("part 2: {}", solve2("inputs/4b"));
 }
 
