85cefa0f4f357a9ec13f626be92c73c34f6a2f99
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-06 19:25:44 +0100

    remove floats, use newton method

--- src/6.rs
+++ src/6.rs
@@ -28,56 +28,31 @@ fn parse2(f: &str) -> (i64, i64) {
         .collect_tuple()
         .unwrap()
 }
-// (t, min_distance)
-// distance = (t - x) * x
-// distance = x*t - x^2
-// min_distance < x*t - x^2
-// 0 < -x^2 + xt - min_distance
-// we can assume there always are solutions (:clueless:)
-// a = 1
-// b = t
-// c = -min_distance
-// Δ = b^2 - 4ac
-// x1 =  (-b - √Δ) /2a
-// x2 =  (-b + √Δ) /2a
-fn roots(a: i64, b: i64, c: i64) -> (f64, f64) {
-    let delta = b * b - 4 * a * c;
-    let sqrt_delta = (delta as f64).sqrt();
-    let twoa = (2 * a) as f64;
-    let minus_b = -b as f64;
-    ((minus_b + sqrt_delta) / twoa, (minus_b - sqrt_delta) / twoa)
-}
-fn find_bound(start:i64, time : i64, distance : i64, above:bool) -> i64 { // can't pass lambda
-    let f = |x:i64| -x*x + x*time - distance; // f
-    let d = |x:i64| -2*x + time; // f'
-    // newthon method
+
+fn find_bound(start: i64, time: i64, distance: i64, slope: i64) -> i64 {
+    // can't pass lambda
+    let f = |x: i64| -x * x + x * time - distance; // f
+    let d = |x: i64| -2 * x + time; // f'
+                                    // newthon method
     let mut prev = -1;
     let mut curr = start;
     while prev != curr {
-        println!("{start} -> {curr} {above}");
         prev = curr;
-        curr = curr - f(curr) / (10*d(curr)); // more stability
+        curr = curr - f(curr) / d(curr); // newton method, more stability
     }
-    if !above {
-        curr -= 10;
-        loop {
-            if f(curr) >0 {return curr}
-            curr += 1;
+    loop {
+        if f(curr) > 0 {
+            return curr;
         }
-    } else {
-        curr += 10;
-        loop {
-            if f(curr) >0 {return curr}
-            curr -= 1;
-        }
+        curr += slope;
     }
 }
+
 fn get_range((time, distance): (i64, i64)) -> (i64, i64) {
-    let bound1 = find_bound(0, time, distance, false);
-    println!("found bound 1");
-    let bound2 = find_bound(bound1*50, time, distance, true);
-    println!("{bound1} {bound2}");
-    (bound1, bound2)
+    (
+        find_bound(0, time, distance, 1),
+        find_bound(time, time, distance, -1),
+    )
 }
 
 fn solve(f: &str) -> i64 {
@@ -86,12 +61,13 @@ fn solve(f: &str) -> i64 {
         .map(|(x, y)| y - x + 1)
         .fold(1, |x, y| x * y)
 }
+
 fn solve2(f: &str) -> i64 {
     let (x, y) = get_range(parse2(f));
     y - x + 1
 }
+
 fn main() {
-    println!("part 1: {:?}", solve("inputs/6a"));
     println!("part 1: {:?}", solve("inputs/6b"));
     println!("part 2: {:?}", solve2("inputs/6b"));
 }
@@ -104,6 +80,15 @@ mod tests {
         assert_eq!((2, 5), get_range((7, 9)));
         assert_eq!((4, 11), get_range((15, 40)));
         assert_eq!((11, 19), get_range((30, 200)));
+
+        assert_eq!((14, 71516), get_range((71530, 940200)));
+
+        assert_eq!((8, 37), get_range((45, 295)));
+        assert_eq!((24, 74), get_range((98, 1734)));
+        assert_eq!((21, 62), get_range((83, 1278)));
+        assert_eq!((26, 47), get_range((73, 1210)));
+
+        assert_eq!((7711543, 38276830), get_range((45988373, 295173412781210)));
     }
     #[test]
     fn part1() {
