2fa89932a0877eb1a458cb66f1c76d49378f0bb7
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-09 02:21:27 +0100

    random changes without much direction, am sleeby

--- src/8.rs
+++ src/8.rs
@@ -18,7 +18,7 @@ fn parse_dir(c: char) -> Dir {
 }
 type Node = (char, char, char);
 
-fn encode_node(x:&str) -> Node {
+fn encode_node(x: &str) -> Node {
     x.chars().collect_tuple().unwrap()
 }
 
@@ -27,42 +27,77 @@ fn parse_line(l: &str) -> (Node, (Node, Node)) {
     (get(0), (get(7), get(12)))
 }
 
-fn follow (direction: Dir, (left, right): (Node, Node)) -> Node {
+fn follow(direction: Dir, (left, right): (Node, Node)) -> Node {
     match direction {
         Dir::Left => left,
-        Dir::Right => right
+        Dir::Right => right,
     }
 }
 
-fn count_steps((directions, nodes): (Vec<Dir>, HashMap<Node, (Node, Node)>), start: Node, end: Node) -> i64{
-    let mut current = start;
-    let mut steps = 0;
-    for dir in directions.into_iter().cycle() {
-        steps += 1;
-        current = follow(dir, *nodes.get(&current).unwrap());
-        if current == end {break}
-    }
-    return steps;
+fn walking<'a>(
+    start: Node,
+    nodes: HashMap<Node, (Node, Node)>,
+    directions: Vec<Dir>,
+) -> impl Iterator<Item = Node> + 'a {
+    directions.into_iter().cycle().scan(start, move |x, dir| {
+        *x = follow(dir, *nodes.get(x)?);
+        Some(*x)
+    })
 }
-fn ending_node(n: &Node) -> bool {
+
+fn count_steps(
+    (directions, nodes): (Vec<Dir>, HashMap<Node, (Node, Node)>),
+    start: Node,
+    end: Node,
+) -> usize {
+    walking(start, nodes, directions)
+        .find_position(|&x| x == end)
+        .unwrap()
+        .0
+        + 1
+}
+
+fn ending_node(n: Node) -> bool {
     n.2 == 'Z'
 }
 fn start_node(n: &Node) -> bool {
     n.2 == 'A'
 }
 
-fn part2((directions, nodes): (Vec<Dir>, HashMap<Node, (Node, Node)>)) -> i64{
-    let mut currents: Vec<Node> = nodes.keys().copied().filter(start_node).collect();
-    let mut steps = 0;
-    for dir in directions.into_iter().cycle() {
-        steps += 1;
-        for c in currents.iter_mut() {
-            *c = follow(dir, *nodes.get(c).unwrap());
+fn count_period(
+    start: Node,
+    nodes: HashMap<Node, (Node, Node)>,
+    directions: Vec<Dir>,
+) -> (usize, usize) {
+    // returns the start, and period of the first found cycle
+    let mut visited: HashMap<(usize, Node), usize> = HashMap::new();
+    // node -> absolutei
+    //
+    let len = directions.len();
+    for (current, node) in walking(start, nodes, directions).enumerate() {
+        let local = current % len;
+        if ending_node(node) {
+            // every time we roll over the directions,
+            // check if we visited that node before
+            match visited.get(&(local, node)) {
+                None => {
+                    visited.insert((local, node), current);
+                }
+                Some(previous) => return (*previous, current - previous),
+            }
         }
-        //println!("state: {:?}", currents);
-        if currents.iter().all(ending_node) {break}
     }
-    return steps;
+    (0, 0)
+}
+
+fn part2((directions, nodes): (Vec<Dir>, HashMap<Node, (Node, Node)>)) -> i64 {
+    let keys: Vec<_> = nodes.keys().copied().filter(start_node).collect();
+    let _x = keys
+        .into_iter()
+        .map(move |start| count_period(start, nodes.to_owned(), directions.to_owned()))
+        .inspect(|x| println!("{:?}", x))
+        .collect_vec();
+    return 3;
 }
 
 fn parse(f: &str) -> (Vec<Dir>, HashMap<Node, (Node, Node)>) {
@@ -77,8 +112,9 @@ fn parse(f: &str) -> (Vec<Dir>, HashMap<Node, (Node, Node)>) {
 fn main() {
     let start = encode_node("AAA");
     let end = encode_node("ZZZ");
-    //println!("part 1: {:?}", count_steps(parse("inputs/8c"), start, end));
-    println!("part 2: {:?}", part2(parse("inputs/8b")));
+    //println!("part 1: {:?}", count_steps(parse("inputs/8b"), start, end));
+    //println!("part 1: {:?}", count_steps(parse("inputs/8b"), start, end));
+    println!("part 2: {:?}", part2(parse("inputs/8c")));
 }
 
 #[cfg(test)]
