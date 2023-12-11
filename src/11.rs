1907d9513767935376e6194109e185c8fcbf74e1
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-11 17:33:07 +0100

    lmao

--- src/11.rs
+++ src/11.rs
@@ -1,11 +1,11 @@
-fn count_distances(d: Vec<usize>) -> usize {
+fn count_distances(d: Vec<usize>, expansion: usize) -> usize {
     let mut ahead: usize = d.iter().sum();
     let mut acc: usize = 0;
     let mut visited = 0;
     for i in d {
         ahead -= i;
         visited += i;
-        acc += visited * ahead * (if i == 0 { 2 } else { 1 })
+        acc += visited * ahead * (if i == 0 { expansion } else { 1 })
     }
     acc
 }
@@ -18,13 +18,15 @@ fn parse(f: &str) -> (Vec<usize>, Vec<usize>) {
         .collect();
     (h, v)
 }
-fn part1(f: &str) -> usize {
+
+fn solve(f: &str, expansion: usize) -> usize {
     let (h, v) = parse(f);
-    count_distances(h) + count_distances(v)
+    count_distances(h, expansion) + count_distances(v, expansion)
 }
 
 fn main() {
-    println!("part 1: {:?}", part1("inputs/11b"));
+    println!("part 1: {:?}", solve("inputs/11b", 2));
+    println!("part 2: {:?}", solve("inputs/11b", 1_000_000));
 }
 
 #[cfg(test)]
@@ -32,18 +34,18 @@ mod tests {
     use crate::*;
     #[test]
     fn solver_test() {
-        assert_eq!(0, count_distances(vec![5]));
-        assert_eq!(0, count_distances(vec![5, 0]));
-        assert_eq!(3, count_distances(vec![1, 3]));
-        assert_eq!(4, count_distances(vec![2, 2]));
-        assert_eq!(4, count_distances(vec![1, 1, 1]));
-        assert_eq!(6, count_distances(vec![1, 2, 1]));
-        assert_eq!(7, count_distances(vec![2, 1, 1]));
-        assert_eq!(12, count_distances(vec![2, 0, 2]));
+        assert_eq!(0, count_distances(vec![5], 2));
+        assert_eq!(0, count_distances(vec![5, 0], 2));
+        assert_eq!(3, count_distances(vec![1, 3], 2));
+        assert_eq!(4, count_distances(vec![2, 2], 2));
+        assert_eq!(4, count_distances(vec![1, 1, 1], 2));
+        assert_eq!(6, count_distances(vec![1, 2, 1], 2));
+        assert_eq!(7, count_distances(vec![2, 1, 1], 2));
+        assert_eq!(12, count_distances(vec![2, 0, 2], 2));
     }
     #[test]
-    fn part1_test() {
-        assert_eq!(374, part1("inputs/11a"));
-        assert_eq!(9623138, part1("inputs/11a"));
+    fn solving_test() {
+        assert_eq!(374, solve("inputs/11a", 2));
+        assert_eq!(9623138, solve("inputs/11b", 2));
     }
 }
