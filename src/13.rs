62bed06f51e5f0c4ca5d398ed5a7fb2de2b9e4cf
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-13 15:46:14 +0100

    part 2

--- src/13.rs
+++ src/13.rs
@@ -1,4 +1,3 @@
-
 use itertools::Itertools;
 
 // https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
@@ -36,69 +35,37 @@ fn parse(f: &str) -> Vec<Field> {
     std::fs::read_to_string(f).unwrap().split("\n\n").map(parse_field).collect()
 }
 
-fn find_reflections<T>(u: Vec<T>) -> Vec<usize>
-where
-    T: PartialEq,
-{
-    u.iter()
-        .tuple_windows()
-        .enumerate()
-        .filter_map(|(e, (a, b))| if a == b { Some(e) } else { None })
-        .collect_vec()
+fn defects(a: &Vec<Tile>, b: &Vec<Tile>) -> usize {
+    std::iter::zip(a, b).filter(|(a, b)| a != b).count()
 }
 
-fn check_reflection<T>(u: Vec<T>, start: usize) -> bool
-where
-    T: PartialEq,
-{
-    for i in 0.. {
-        if i > start {
-            return true;
-        }
+fn count_defects(u: Field, start: usize) -> usize {
+    let mut acc = 0;
+    for i in 0..=start {
         match (u.get(start - i), u.get(start + i + 1)) {
-            (Some(x), Some(y)) if x != y => return false,
-            (Some(x), Some(y)) if x == y => {}
-            _ => return true,
+            (Some(x), Some(y)) => acc += defects(x, y),
+            _ => break,
         }
     }
-    panic!()
+    acc
 }
 
-fn above_reflection<T>(u: &Vec<T>) -> usize where T:PartialEq, T:Clone, {
-    match find_reflections(u.to_owned()).iter().copied().filter(|i|  check_reflection(u.to_owned(), *i)).next() {
+fn above_reflection(u: &Field, defects: usize) -> usize {
+    match (0..(u.len() - 1)).find(|i| count_defects(u.to_owned(), *i) == defects) {
         Some(x) => x + 1,
-        None => 0
+        None => 0,
     }
 }
 
-fn summarize(u: Field) -> usize {
-    100 * above_reflection(&u) + above_reflection(&transpose(u))
+fn summarize(u: Field, defects: usize) -> usize {
+    100 * above_reflection(&u, defects) + above_reflection(&transpose(u), defects)
 }
-fn  part1(f: &str) -> usize {
-    parse(f).into_iter().map(summarize).inspect(|x| println!("{:?}", x)).sum()
+
+fn solve(f: &str, defects: usize) -> usize {
+    parse(f).into_iter().map(|x| summarize(x, defects)).sum()
 }
 
 fn main() {
-    println!("part1: {}", part1("inputs/13b"));
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::*;
-    #[test]
-    fn reflection_test() {
-        assert_eq!(vec![2], find_reflections(vec![1, 2, 3, 3, 5, 7]));
-        assert_eq!(vec![0, 2], find_reflections(vec![2, 2, 3, 3, 5, 7]));
-    }
-    #[test]
-    fn part1_test () {
-        assert_eq!(1, above_reflection(&vec![2, 2]));
-        assert_eq!(2, above_reflection(&vec![3, 2, 2]));
-        assert_eq!(2, above_reflection(&vec![3, 2, 2, 3]));
-        assert_eq!(1, above_reflection(&vec![2, 2, 3]));
-        assert_eq!(1, above_reflection(&vec![2, 2, 3]));
-        assert_eq!(1, above_reflection(&vec![2, 2, 3]));
-        assert_eq!(0, above_reflection(&vec![2, 3, 1, 1, 3, 22, 33]));
-        assert_eq!(5, above_reflection(&vec![4, 3, 2, 1, 0, 0, 1, 2, 3]));
-    }
+    println!("part1: {}", solve("inputs/13b", 0));
+    println!("part2: {}", solve("inputs/13b", 1));
 }
