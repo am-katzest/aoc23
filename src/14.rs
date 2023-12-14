d184ac69b8345eb6ec310f9088258cc331bdd900
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-14 18:42:52 +0100

    day 14

--- src/14.rs
+++ src/14.rs
@@ -1,6 +1,5 @@
 use itertools::Itertools;
-use std::fs::read_to_string;
-
+use std::{collections::HashMap, fs::read_to_string};
 
 // https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
 fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
@@ -17,12 +16,11 @@ fn reverse(m: Map) -> Map {
     m.into_iter().rev().collect_vec()
 }
 
-
 fn flip(m: Map) -> Map {
     m.into_iter().map(|x| x.into_iter().rev().collect_vec()).collect_vec()
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Round,
@@ -45,33 +43,36 @@ fn shove_line_right(m: &mut Row) {
     let mut stones = 0;
     for i in 0..m.len() {
         match m[i] {
-                Tile::Square => {
-                    for i in (i-stones)..i {
-                        m[i] = Tile::Round;
-                    }
-                    stones = 0;
+            Tile::Square => {
+                for i in (i - stones)..i {
+                    m[i] = Tile::Round;
                 }
-                Tile::Round => {stones += 1; m[i] = Tile::Empty},
-                _ => {},
+                stones = 0;
             }
+            Tile::Round => {
+                stones += 1;
+                m[i] = Tile::Empty
+            }
+            _ => {}
+        }
     }
-    for i in (m.len()-stones)..m.len() {
+    for i in (m.len() - stones)..m.len() {
         m[i] = Tile::Round;
     }
 }
-fn shr (a:&mut Map) {
+fn shr(a: &mut Map) {
     for i in 0..a.len() {
         shove_line_right(&mut a[i]);
     }
 }
 
-fn right(m:Map) -> Map {
+fn right(m: Map) -> Map {
     let mut a = m.clone();
     shr(&mut a);
     a
 }
 
-fn left(m:Map) -> Map {
+fn left(m: Map) -> Map {
     let mut a = flip(m);
     shr(&mut a);
     flip(a)
@@ -89,10 +90,9 @@ fn down(m: Map) -> Map {
     transpose(a)
 }
 
-fn spin(m:Map) -> Map {
+fn spin(m: Map) -> Map {
     right(down(left(up(m))))
 }
-
 
 fn parse(f: &str) -> Map {
     read_to_string(f)
@@ -107,30 +107,45 @@ fn count_stones(u: &Row) -> usize {
 }
 
 fn calc_load(m: Map) -> usize {
-    m.iter().rev().enumerate().map(|(i, x)| (i+1) * count_stones(x)).sum()
+    m.iter().rev().enumerate().map(|(i, x)| (i + 1) * count_stones(x)).sum()
 }
 
-fn part1(f:&str) -> usize {
+fn part1(f: &str) -> usize {
     calc_load(up(parse(f)))
 }
 
-fn main() {
-    println!("part1: {}", part1("inputs/14a"));
-    println!("part1: {}", part1("inputs/14b"));
+fn speen(m: Map, n: usize) -> Map {
+    let mut a = m.clone();
+    for _ in 0..n {
+        a = spin(a);
+    }
+    a
 }
 
-fn print(m: Map) {
-    for row in m {
-        for i in row {
-            let a = match i {
-                Tile::Empty => ".",
-                Tile::Square => "#",
-                Tile::Round => "O"
-            };
-            print!("{a}");
+fn part2(f: &str) -> usize {
+    let mut visited: HashMap<Map, usize> = HashMap::new();
+    let mut a = parse(f);
+    let target = 1_000_000_000;
+    for i in 0..target {
+        match visited.get(&a) {
+            None => {
+                visited.insert(a.to_owned(), i);
+            }
+            Some(previous) => {
+                let metacycle = i - previous;
+                let remaining = target - i;
+                let rem = remaining % metacycle;
+                return calc_load(speen(a, rem));
+            }
         }
-        println!("");
+        a = spin(a);
     }
+    panic!()
+}
+
+fn main() {
+    println!("part1: {}", part1("inputs/14b"));
+    println!("part2: {}", part2("inputs/14b"));
 }
 
 #[cfg(test)]
@@ -138,27 +153,33 @@ mod tests {
     use crate::*;
     #[test]
     fn shove_right_test() {
-
         let mut a = vec![Tile::Round, Tile::Empty];
         let b = vec![Tile::Empty, Tile::Round];
-        shove_line_right(& mut a);
+        shove_line_right(&mut a);
         assert_eq!(a, b);
 
         let mut a = vec![Tile::Round, Tile::Round, Tile::Empty, Tile::Empty];
-        let b = vec![Tile::Empty,Tile::Empty, Tile::Round, Tile::Round];
-        shove_line_right(& mut a);
+        let b = vec![Tile::Empty, Tile::Empty, Tile::Round, Tile::Round];
+        shove_line_right(&mut a);
         assert_eq!(a, b);
 
         let mut a = vec![Tile::Round, Tile::Empty, Tile::Square, Tile::Empty];
         let b = vec![Tile::Empty, Tile::Round, Tile::Square, Tile::Empty];
-        shove_line_right(& mut a);
+        shove_line_right(&mut a);
         assert_eq!(a, b);
-
     }
     #[test]
     fn shove_north_test() {
         assert_eq!(parse("inputs/14an"), up(parse("inputs/14a")));
         assert_eq!(parse("inputs/14a1"), spin(parse("inputs/14a")));
         assert_eq!(parse("inputs/14a2"), spin(spin(parse("inputs/14a"))));
+    }
+    #[test]
+    fn part1_test() {
+        assert_eq!(136, part1("inputs/14a"));
+    }
+    #[test]
+    fn part2_test() {
+        assert_eq!(64, part2("inputs/14a"));
     }
 }
