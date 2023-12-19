5b8213ae418348cacd1ecbdb1ccb16e7d427e791
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-19 01:48:04 +0100

    meow

--- src/18.rs
+++ src/18.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use std::fs::read_to_string;
 use substring::Substring;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Axis {
     Horizontal,
     Vertical,
@@ -92,12 +92,97 @@ fn parse(f: &str, parse_line: fn(&str) -> Instruction) -> Vec<Instruction> {
     read_to_string(f).unwrap().lines().map(parse_line).collect()
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Segment {
+    altitude: isize,
     axis: Axis,
     start: isize,
     end: isize,
-    altitude: isize,
+}
+fn relevant(s: Segment, x: isize) -> bool {
+    x >= s.start && x <= s.end
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Side {
+    Inside,
+    Outside,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Action {
+    Enter,
+    Leave,
+    Neither,
+}
+
+fn edge(s: Segment, x: isize) -> Side {
+    if relevant(s, x) {
+        if x == s.start {
+            return Side::Left;
+        }
+        if x == s.end {
+            return Side::Right;
+        }
+        return Side::Inside;
+    }
+    return Side::Outside;
+}
+
+fn height(ss: &Vec<Segment>, x: isize) -> isize {
+    let intersections = ss.iter().copied().filter(|&s| relevant(s, x)).sorted().map(|s| (edge(s, x), s.altitude));
+    let mut acc = 0;
+    let mut state = Side::Outside;
+    let mut entered = 0;
+    for (edge, alt) in intersections {
+        let a = match (state, edge) {
+            (_, Side::Outside) | (Side::Left, Side::Inside) | (Side::Right, Side::Inside) => {
+                panic!();
+            }
+            (Side::Outside, e) => {
+                state = e; //TODO
+                Action::Enter
+            }
+            // leaving side
+            (Side::Left, Side::Left) | (Side::Right, Side::Right) => {
+                state = Side::Outside;
+                Action::Leave
+            }
+            // entering into main body (while being on side)
+            (Side::Left, Side::Right) | (Side::Right, Side::Left) => {
+                state = Side::Inside;
+                Action::Neither
+            }
+            (Side::Inside, Side::Inside) => {
+                // leaving
+                state = Side::Outside;
+                Action::Leave
+            }
+            (Side::Inside, Side::Left) => {
+                // starting to leave
+                state = Side::Right;
+                Action::Neither
+            }
+            (Side::Inside, Side::Right) => {
+                // starting to leave
+                state = Side::Left;
+                Action::Neither
+            }
+        };
+        match a {
+            Action::Enter => {
+                entered = alt;
+            }
+            Action::Leave => {
+                acc += alt - entered + 1;
+            }
+            Action::Neither => {}
+        }
+        //println!("{:?}, {} (am {:?})", edge, alt, state);
+    }
+    acc
 }
 
 fn solve(instr: Vec<Instruction>) -> isize {
@@ -106,16 +191,17 @@ fn solve(instr: Vec<Instruction>) -> isize {
     // we are scanning left to right
     let changes = breakpoints(&segments, Axis::Vertical);
     let bars = bars(&segments, Axis::Horizontal);
-    println!("{:?}", bars);
-    3
+    let mut acc = 0;
+    for (i,i1) in changes.iter().copied().tuple_windows() {
+        acc += height(&bars, i); // at the edge
+        acc += height(&bars, i+1) * (i1 - i - 1); // between
+    }
+    acc += height(&bars, *changes.last().unwrap());
+    acc
 }
 
-fn bars(segments: &Vec<Segment>, a:Axis) -> Vec<Segment> {
-    segments
-        .iter()
-        .copied()
-        .filter(|x| x.axis == a)
-        .collect_vec()
+fn bars(segments: &Vec<Segment>, a: Axis) -> Vec<Segment> {
+    segments.iter().copied().filter(|x| x.axis == a).collect_vec()
 }
 
 fn breakpoints(segments: &Vec<Segment>, a: Axis) -> Vec<isize> {
