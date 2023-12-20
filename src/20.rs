290da2bc1757f893b9aa0067d18bb46ec56ec981
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-20 13:24:41 +0100

    meow

--- src/20.rs
+++ src/20.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, fs::read_to_string};
 
+use itertools::Itertools;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Pulse {
     High,
@@ -8,58 +10,105 @@ enum Pulse {
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum FlipFlopState {
-    On, Off
+    On,
+    Off,
 }
 
-type ConjState = Vec<(String, Pulse)>; // hashmap is an overkill and not hashable
+type ConjState = Vec<(String, Pulse)>; // hashmap is an overkill and not hashable, needs to be initialized beforehand
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Module {
     Broadcast,
     FlipFlop(FlipFlopState),
-    Conj(ConjState)
+    Conj(ConjState),
 }
 
-fn flip(s:FlipFlopState) -> FlipFlopState {
+fn flip(s: FlipFlopState) -> FlipFlopState {
     match s {
         FlipFlopState::On => FlipFlopState::Off,
         FlipFlopState::Off => FlipFlopState::On,
     }
 }
 
-fn ffpulse(s:FlipFlopState) -> Pulse {
+fn ffpulse(s: FlipFlopState) -> Pulse {
     match s {
         FlipFlopState::On => Pulse::Low,
         FlipFlopState::Off => Pulse::High,
     }
 }
 
-fn apply_flip_flop(s:FlipFlopState, p: Pulse)  -> (FlipFlopState, Option<Pulse>){
+fn apply_flip_flop(s: FlipFlopState, p: Pulse) -> (FlipFlopState, Option<Pulse>) {
     match p {
         Pulse::High => (s, None),
         Pulse::Low => (flip(s), Some(ffpulse(s))),
     }
 }
 
-fn apply_conj(s:ConjState, p: Pulse) -> (ConjState, Option<Pulse>) {
-    todo!()
+fn apply_conj(s: ConjState, source: String, p: Pulse) -> (ConjState, Option<Pulse>) {
+    let s: ConjState = s.into_iter().map(|(n, o)| if n == source { (n, p) } else { (n, o) }).collect_vec();
+    if s.iter().all(|(_, s)| *s == Pulse::High) {
+        (s, Some(Pulse::Low))
+    } else {
+        (s, Some(Pulse::High))
+    }
 }
 
 // what module outputs
-fn apply(m: Module, p:Pulse) -> (Module, Option<Pulse>){
+fn apply(m: Module, source: String, p: Pulse) -> (Module, Option<Pulse>) {
     // too lazy to figure out how to do polymorphism the *not-hurting* way
     match m {
         Module::Broadcast => (Module::Broadcast, Some(p)),
         Module::FlipFlop(s) => {
             let (s, o) = apply_flip_flop(s, p);
-            (Module::FlipFlop(s), o)},
+            (Module::FlipFlop(s), o)
+        }
         Module::Conj(s) => {
-            let (s, o) = apply_conj(s, p);
-            (Module::Conj(s), o)},
+            let (s, o) = apply_conj(s, source, p);
+            (Module::Conj(s), o)
+        }
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+enum ModuleType { // just for parsing
+    Broadcast, FlipFlop, Conj
+}
 
+fn pre_parse_module(line: &str) -> (ModuleType, String, Vec<String>) {
+    // %jf -> cr, dn
+    let kind = match line.chars().next().unwrap() {
+        '&' => ModuleType::Conj,
+        '%' => ModuleType::FlipFlop,
+        _ => ModuleType::Broadcast,
+    };
+    let mut tmp = line
+        .split(|x| x == ' ' || x == '-' || x == '>' || x == ',' || x == '&' || x == '%')
+        .filter(|x| x.len() > 0)
+        .map(|x| String::from(x));
+    let first = tmp.next().unwrap();
+    (kind, first, tmp.collect_vec())
+}
+
+fn parse(f: &str) {
+    let content = read_to_string(f).unwrap();
+    let modules = content.lines().map(pre_parse_module).collect_vec();
+
+    let mut inputs: HashMap<String, Vec<String>> = HashMap::new();
+    for (_, sender, outputs) in modules.clone() {
+        for o in outputs {
+            match inputs.get_mut(&o) {
+                Some(previous) => {
+                    (*previous).push(sender.clone());
+                }
+                None => {
+                    inputs.insert(o, vec![sender.clone()]);
+                }
+            }
+        }
+    }
+    println!("{:?}", modules);
+    println!("{:?}", inputs);
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Machine {
@@ -67,6 +116,6 @@ struct Machine {
     targets: Vec<String>,
 }
 
-fn main(){
-
+fn main() {
+    parse("inputs/20a")
 }
