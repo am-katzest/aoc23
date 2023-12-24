6c9b8dfc053a27b36ace6a02182f09a4662de0e4
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-24 14:59:06 +0100

    part 1, ofc borked

--- src/24.rs
+++ src/24.rs
@@ -30,21 +30,21 @@ fn on_the_line_xy(location: Vector, hail: Hail) -> bool {
     assert!(hail.velocity.x != 0.); // TODO
     let diff = location.x - hail.position.x;
     if diff % hail.velocity.x != 0. {
-        return false
+        return false;
     }
     let time = diff / hail.velocity.x;
     if time < 0. {
-        return false
+        return false;
     }
-    let  expected_y_pos = time * hail.velocity.y + hail.position.y;
+    let expected_y_pos = time * hail.velocity.y + hail.position.y;
     if expected_y_pos != location.y {
-        return false
+        return false;
     }
-    return true
+    return true;
 }
 type MM = (f64, f64);
 
-fn within(a: f64, (min, max): MM) -> bool{
+fn within(a: f64, (min, max): MM) -> bool {
     min >= a && a <= max
 }
 
@@ -52,7 +52,7 @@ fn within_borders_xy(loc: Vector, mm: MM) -> bool {
     within(loc.x, mm) && within(loc.y, mm)
 }
 
-fn intersect_xy(a: Hail, b:Hail, mm: MM)  -> bool {
+fn intersect_xy(a: Hail, b: Hail, mm: MM) -> bool {
     match intersection_xy(a, b) {
         Some(point) => on_the_line_xy(point, a) && on_the_line_xy(point, b) && within_borders_xy(point, mm),
         None => false,
@@ -71,7 +71,8 @@ fn intersection_xy(a: Hail, b: Hail) -> Option<Vector> {
     let pxnom = (p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.x * p4.y - p3.y - p4.y);
     let pynom = (p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x * p4.y - p3.y - p4.y);
     let denom = ((p1.x - p2.x) * (p3.y - p4.y)) - ((p1.y - p2.y) * (p3.x - p4.x));
-    if denom == 0. { // check some smarter way
+    if denom == 0. {
+        // check some smarter way
         return None;
     }
     Some(Vector {
@@ -96,13 +97,16 @@ fn parse(f: &str) -> Vec<Hail> {
     std::fs::read_to_string(f).unwrap().lines().map(parse_hail).collect()
 }
 
-fn part1(hails: Vec<Hail>, mm: MM) -> usize {
-    todo!();
+fn part1(hails: &Vec<Hail>, mm: MM) -> usize {
+    hails
+        .iter()
+        .cartesian_product(hails.iter())
+        .filter(|(a, b)| a != b && intersect_xy(**a, **b, mm))
+        .count()
 }
 
-
 fn main() {
-    println!("{:?}", parse("inputs/24a"));
+    println!("{:?}", part1(&parse("inputs/24a"), (7., 27.)));
 }
 
 #[cfg(test)]
@@ -115,13 +119,14 @@ mod tests {
             Some(mkxy(1., 0.)),
             intersection_xy(
                 Hail {
-                velocity: mkxy(0., -1.),
-                position: mkxy(1., 1.)
-            },
-            Hail {
-                velocity: mkxy(1., 0.),
-                position: mkxy(0., 0.)
-            })
+                    velocity: mkxy(0., -1.),
+                    position: mkxy(1., 1.)
+                },
+                Hail {
+                    velocity: mkxy(1., 0.),
+                    position: mkxy(0., 0.)
+                }
+            )
         );
     }
 }
