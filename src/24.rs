be86a924f455717ce8695782e4b00cd90aa942fb
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-24 14:54:28 +0100

    eughh...

--- src/24.rs
+++ src/24.rs
@@ -2,17 +2,17 @@ use std::collections::{HashMap, HashSet, VecDeque};
 
 use itertools::{Itertools, Position};
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 struct Vector {
-    x: i32,
-    y: i32,
-    z: i32,
+    x: f64,
+    y: f64,
+    z: f64,
 }
 
 fn parse_vector(l: &str) -> Vector {
     let (x, y, z) = l
         .split(|x| x == ' ' || x == ',')
-        .filter_map(|x| x.parse::<i32>().ok())
+        .filter_map(|x| x.parse::<f64>().ok())
         .collect_tuple()
         .unwrap();
     Vector { x, y, z }
@@ -27,13 +27,13 @@ fn add(a: Vector, b: Vector) -> Vector {
 }
 
 fn on_the_line_xy(location: Vector, hail: Hail) -> bool {
-    assert!(hail.velocity.x != 0); // TODO
+    assert!(hail.velocity.x != 0.); // TODO
     let diff = location.x - hail.position.x;
-    if diff % hail.velocity.x != 0 {
+    if diff % hail.velocity.x != 0. {
         return false
     }
     let time = diff / hail.velocity.x;
-    if time < 0 {
+    if time < 0. {
         return false
     }
     let  expected_y_pos = time * hail.velocity.y + hail.position.y;
@@ -42,9 +42,9 @@ fn on_the_line_xy(location: Vector, hail: Hail) -> bool {
     }
     return true
 }
-type MM = (i32, i32);
+type MM = (f64, f64);
 
-fn within(a: i32, (min, max): MM) -> bool{
+fn within(a: f64, (min, max): MM) -> bool{
     min >= a && a <= max
 }
 
@@ -65,18 +65,19 @@ fn line_to_points(h: Hail) -> (Vector, Vector) {
 
 // would rather not do slopes, from https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
 fn intersection_xy(a: Hail, b: Hail) -> Option<Vector> {
+    // TODO: switch to simpler method
     let (p1, p2) = line_to_points(a);
     let (p3, p4) = line_to_points(b);
     let pxnom = (p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.x * p4.y - p3.y - p4.y);
     let pynom = (p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x * p4.y - p3.y - p4.y);
     let denom = ((p1.x - p2.x) * (p3.y - p4.y)) - ((p1.y - p2.y) * (p3.x - p4.x));
-    if denom == 0 {
+    if denom == 0. { // check some smarter way
         return None;
     }
     Some(Vector {
         x: pxnom / denom,
         y: pynom / denom,
-        z: 0,
+        z: 0.,
     })
 }
 
@@ -85,7 +86,7 @@ fn parse_hail(h: &str) -> Hail {
     Hail { position, velocity }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 struct Hail {
     position: Vector,
     velocity: Vector,
@@ -95,8 +96,8 @@ fn parse(f: &str) -> Vec<Hail> {
     std::fs::read_to_string(f).unwrap().lines().map(parse_hail).collect()
 }
 
-fn part1(hails: Vec<Hail>, mm: MM) -> i32 {
-
+fn part1(hails: Vec<Hail>, mm: MM) -> usize {
+    todo!();
 }
 
 
@@ -109,17 +110,17 @@ mod tests {
     use crate::*;
     #[test]
     fn linification_test() {
-        let mkxy = |x, y| Vector { x, y, z: 0 };
+        let mkxy = |x, y| Vector { x, y, z: 0. };
         assert_eq!(
-            Some(mkxy(1, 0)),
+            Some(mkxy(1., 0.)),
             intersection_xy(
                 Hail {
-                velocity: mkxy(0, -1),
-                position: mkxy(1, 1)
+                velocity: mkxy(0., -1.),
+                position: mkxy(1., 1.)
             },
             Hail {
-                velocity: mkxy(1, 0),
-                position: mkxy(0, 0)
+                velocity: mkxy(1., 0.),
+                position: mkxy(0., 0.)
             })
         );
     }
